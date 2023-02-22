4022893bcbd468c91131dd3756e90e1a9316cdec
seenstevo <seenstevo@protonmail.com>
2023-02-22 14:20:08 +0100

    create_multiclass df and plot functions

--- setup.py
+++ setup.py
@@ -2,17 +2,19 @@ from distutils.core import setup
 
 setup(
   name = 'toolkit',
-  packages = find_packages(),
+  packages = ['toolkit'],
   version = '0.1',
   license = 'MIT',
   description = 'Helper functions for all stages of the machine learning model building process',
   author = 'TheBridgeMachineLearningPythonLibrary',
   author_email = 'seenstevol@protonmail.com',
   url = 'https://github.com/TheBridgeMachineLearningPythonLibrary/MachineLearningToolKit',
-  download_url = '',    # I explain this later on
+  download_url = 'https://github.com/TheBridgeMachineLearningPythonLibrary/MachineLearningToolKit/archive/refs/tags/v_01.tar.gz',
   keywords = ['machine learning', 'data visualization', 'data processing', 'sklean', 'pandas'],
-  install_requires=[            # I get to this in a second
-          ''
+  install_requires=[
+          'pandas',
+          'numpy',
+          'imblearn'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',
--- toolkit/machine_learning.py
+++ toolkit/machine_learning.py
@@ -1,6 +1,7 @@
 from sklearn.preprocessing import PolynomialFeatures
 import pandas as pd
-from typing import List
+import numpy as np
+from typing import List, Union
 
 
 def ignore_columns_polyfeatures(X: pd.DataFrame, variables_to_ignore: List[str], n: int) -> pd.DataFrame:
@@ -35,3 +36,40 @@ def ignore_columns_polyfeatures(X: pd.DataFrame, variables_to_ignore: List[str],
     X_poly_features = pd.DataFrame(poly_array, columns = poly_features_names)
 
     return pd.concat([X_ignore, X_poly_features], axis = 1)
+
+
+def create_multiclass_prediction_df(model, class_names: List[str], X_test: Union[pd.DataFrame, np.ndarray], y_test: Union[list, np.ndarray], only_wrong: bool = False) -> pd.DataFrame:
+    '''
+    This will generate a dataframe from the predictions of a model on X_test for easy analysis of model performance
+
+    Parameters
+    ----------
+    model : keras model
+        It is the dataset we want to selectively create polynomial features
+    class_names : List[str]
+        a list of the class names which must be in order that relates to the numbers in y_test
+    X_test : pd.DataFrame or np.ndarray
+        The data for X_test
+    y_test : list or np.ndarray
+        The y_test with the numeric values of the class names
+    only_wrong : bool
+        defaults to False but if set to True, only incorrect predictions will be kept
+    
+    Return
+    ----------
+        df : Dataframe containing the predictions from model.predict along with the top prediction and actual labels
+    
+    '''
+    # make the predictions with model on X_test
+    model_predictions_df = round(pd.DataFrame(model.predict(X_test)), 2)
+    # assign class label names as columns
+    model_predictions_df.columns = class_names
+    # create top prediction column
+    model_predictions_df['Top Prediction'] = model_predictions_df.apply(lambda x: x.idxmax(), axis = 1)
+    # create class label column
+    model_predictions_df['Label'] = [model_predictions_df.columns[y_class] for y_class in y_test]
+    # filter for model errors if "only_wrong == True"
+    if only_wrong:
+        model_predictions_df = model_predictions_df[model_predictions_df['Top Prediction'] != model_predictions_df['Label']]
+    # return the dataframe
+    return model_predictions_df   
