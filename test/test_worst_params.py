6024a86d1df5a336a1e5349c44ef0c7ea0a51c99
RamonFCerezo <117541707+RamonFCerezo@users.noreply.github.com>
2023-02-22 21:22:51 +0100

    ramon test

---
+++ test/test_worst_params.py
@@ -0,0 +1,18 @@
+from worst_params import worst_params
+import pytest
+import numpy as np
+from imports_worst_params import clf_cv_results, df
+from sklearn.svm import SVC
+import json
+
+
+@pytest.mark.parametrize(
+        "input_a, expected",
+        [
+            (df, ("{'C': 5, 'degree': 3, 'gamma': 'auto', 'kernel': 's'}", 0.5)),
+            (clf_cv_results, ("{'classifier': SVC(), 'classifier__C': 0.001}", 0.48333333))
+        ]
+)
+
+def test_worst_params_multi(input_a, expected):
+    assert worst_params(input_a) == expected
