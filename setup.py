f75df71842281e781e06cd098c51299eb988070b
seenstevo <seenstevo@protonmail.com>
2023-02-22 15:55:25 +0100

    test0_2

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
 setup(
-  name = 'toolkit',
-  packages = ['toolkit'],
-  version = '0.1',
+  name = 'thebridgemltoolkit',
+  packages = ['thebridgemltoolkit'],
+  version = '0.2',
   license = 'MIT',
   description = 'Helper functions for all stages of the machine learning model building process',
   author = 'TheBridgeMachineLearningPythonLibrary',
   author_email = 'seenstevol@protonmail.com',
   url = 'https://github.com/TheBridgeMachineLearningPythonLibrary/MachineLearningToolKit',
-  download_url = 'https://github.com/TheBridgeMachineLearningPythonLibrary/MachineLearningToolKit/archive/refs/tags/v_01.tar.gz',
+  download_url = 'https://github.com/TheBridgeMachineLearningPythonLibrary/MachineLearningToolKit/archive/refs/tags/v_02.tar.gz',
   keywords = ['machine learning', 'data visualization', 'data processing', 'sklearn', 'pandas'],
   install_requires=[
           'pandas',
@@ -20,8 +20,6 @@ setup(
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',
-    'Intended Audience :: Data Scientists',
-    'Topic :: Data Science :: Machine Learning Tools',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.7',
   ],
---
+++ thebridgemltoolkit/__init__.py
@@ -0,0 +1,4 @@
+from .thebridgemltoolkit.machine_learning import *
+from .thebridgemltoolkit.data_analysis import *
+from .thebridgemltoolkit.plot import *
+from .thebridgemltoolkit.data_processing import *
