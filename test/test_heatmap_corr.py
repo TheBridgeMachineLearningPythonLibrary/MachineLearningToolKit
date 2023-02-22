0a9ceb89fcdab8200ee442661b04d8a4dafce6c0
MiguelDeFrutos <116830299+Migueldfr@users.noreply.github.com>
2023-02-22 21:21:12 +0100

    Update test_heatmap_corr.py

--- test/test_heatmap_corr.py
+++ test/test_heatmap_corr.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-from heatmap_corr import heatmap
+from toolkit.plot import heatmap
 
 def test_heatmap():
     df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9], 'target': [10, 20, 30]})
