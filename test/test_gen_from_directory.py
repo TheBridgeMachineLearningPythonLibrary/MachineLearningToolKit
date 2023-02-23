7eee2ee598f3810394dfea6c3186a83c946355d2
Enrique Moya <enriquemoygar@gmail.com>
2023-02-23 13:34:01 +0100

    image_data_gen

---
+++ test/test_gen_from_directory.py
@@ -0,0 +1,17 @@
+from toolkit.data_processing import ImageDataGen
+
+def test_gen_from_directory():
+    max_image_count = 100
+
+    datagen = ImageDataGen()
+    generator = datagen.gen_from_directory(
+        './image_data_gen_test_data/',
+        max_image_count=max_image_count,
+        batch_size=1,
+        save_to_dir='./image_data_gen_test_data/aug'
+    )
+
+    assert len(generator) == 165
+    X, y = generator.next()
+    assert X.shape == (1, 256, 256, 3)
+    assert y.shape == (1, 2)
